2c74d949cfb2fdd1072f25ab7fd20987c637575a
Shybert <25749775+Shybert@users.noreply.github.com>
2020-07-15 17:36:23 +0200

    Implemented Add for Points and Vectors

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     println!("Hello, world!");
 }
 
+use std::ops::Add;
+
 fn approx_equals(a: f64, b: f64) -> bool {
     return (a - b).abs() < 0.00001;
 }
@@ -12,7 +14,6 @@ struct Point {
     y: f64,
     z: f64,
 }
-
 impl Point {
     fn new(x: f64, y: f64, z: f64) -> Point {
         return Point { x, y, z };
@@ -26,6 +27,13 @@ impl PartialEq<Point> for Point {
             && approx_equals(self.z, other.z);
     }
 }
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        return Self::new(self.x + other.x, self.y + other.y, self.z + other.z);
+    }
+}
 
 #[derive(Debug)]
 struct Vector {
@@ -34,7 +42,6 @@ struct Vector {
     z: f64,
     w: f64,
 }
-
 impl Vector {
     fn new(x: f64, y: f64, z: f64, w: f64) -> Vector {
         return Vector { x, y, z, w };
@@ -47,6 +54,18 @@ impl PartialEq<Vector> for Vector {
             && approx_equals(self.y, other.y)
             && approx_equals(self.z, other.z)
             && approx_equals(self.w, other.w);
+    }
+}
+impl Add for Vector {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        return Self::new(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+            self.w + other.w,
+        );
     }
 }
 
@@ -62,6 +81,14 @@ mod tests {
     }
 
     #[test]
+    fn point_addition() {
+        assert_eq!(
+            Point::new(3.0, -2.0, 5.0) + Point::new(-2.0, 3.0, 1.0),
+            Point::new(1.0, 1.0, 6.0)
+        )
+    }
+
+    #[test]
     fn vector_equality() {
         assert_eq!(
             Vector::new(4.0, -4.0, 3.0, -42.0),
@@ -72,5 +99,13 @@ mod tests {
             Vector::new(1.000000001, 0.0, 0.0, 0.0),
             Vector::new(1.0, 0.0, 0.0, 0.0)
         );
+    }
+
+    #[test]
+    fn vector_addition() {
+        assert_eq!(
+            Vector::new(3.0, -2.0, 5.0, 0.0) + Vector::new(-2.0, 3.0, 1.0, 0.0),
+            Vector::new(1.0, 1.0, 6.0, 0.0)
+        )
     }
 }
