22a35ac6564443fe8dd456b2a8dd3b3d9976db2c
Shybert <25749775+Shybert@users.noreply.github.com>
2020-07-16 09:27:41 +0200

    Created the Canvas struct

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,6 +1,6 @@
 use super::utils;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Color {
   r: f64,
   g: f64,
@@ -20,6 +20,29 @@ impl PartialEq<Self> for Color {
   }
 }
 
+struct Canvas {
+  width: usize,
+  height: usize,
+  pixels: Vec<Color>,
+}
+impl Canvas {
+  fn new(width: usize, height: usize) -> Self {
+    return Canvas {
+      width,
+      height,
+      pixels: vec![Color::new(0., 0., 0.); width * height],
+    };
+  }
+
+  fn get_width(&self) -> usize {
+    return self.width;
+  }
+
+  fn get_height(&self) -> usize {
+    return self.height;
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -30,4 +53,19 @@ mod tests {
 
     assert_eq!(Color::new(1.000000001, 0., 0.), Color::new(1., 0., 0.));
   }
+
+  #[test]
+  fn canvas_get_width() {
+    assert_eq!(Canvas::new(10, 20).get_width(), 10);
+  }
+
+  #[test]
+  fn canvas_get_height() {
+    assert_eq!(Canvas::new(10, 20).get_height(), 20);
+  }
+
+  // #[test]
+  // fn canvas_get_pixel() {
+  //   assert_eq!(Canvas::new(10, 20).get_pixel(0, 0), Color::new(0., 0., 0.));
+  // }
 }
