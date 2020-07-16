59b6a446a3234179a96df173cd91ee9ab957c1d6
Shybert <25749775+Shybert@users.noreply.github.com>
2020-07-16 19:10:38 +0200

    Added clamp_number() util, with tests

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,43 @@
 pub fn approx_equals(a: f64, b: f64) -> bool {
   return (a - b).abs() < 0.00001;
 }
+
+pub fn clamp_number(number: f64, min: f64, max: f64) -> f64 {
+  return if number < min {
+    min
+  } else if number > max {
+    max
+  } else {
+    number
+  };
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn clamp_number_above_max() {
+    assert_eq!(clamp_number(256., 0., 255.), 255.);
+  }
+
+  #[test]
+  fn clamp_number_at_max() {
+    assert_eq!(clamp_number(255., 0., 255.), 255.);
+  }
+
+  #[test]
+  fn clamp_number_below_min() {
+    assert_eq!(clamp_number(-1., 0., 255.), 0.);
+  }
+
+  #[test]
+  fn clamp_number_at_min() {
+    assert_eq!(clamp_number(0., 0., 255.), 0.);
+  }
+
+  #[test]
+  fn clamp_number_between() {
+    assert_eq!(clamp_number(128., 0., 255.), 128.);
+  }
+}
