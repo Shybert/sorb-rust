8e38687244fe83de40814b9a21682960e8ef6372
Shybert <25749775+Shybert@users.noreply.github.com>
2020-07-19 07:28:03 +0200

    Added function for creating a matrix from provided elements

--- src/geometry/matrix.rs
+++ src/geometry/matrix.rs
@@ -12,6 +12,10 @@ impl Matrix {
       ],
     };
   }
+
+  fn from(elements: [[f64; 4]; 4]) -> Self {
+    return Self { elements };
+  }
 }
 
 #[cfg(test)]
@@ -29,5 +33,17 @@ mod tests {
         [0., 0., 0., 1.],
       ]
     );
+  }
+
+  #[test]
+  fn from() {
+    let elements = [
+      [1., 2., 3., 4.],
+      [5.5, 6.5, 7.5, 8.5],
+      [9., 10., 11., 12.],
+      [13.5, 14.5, 15.5, 16.5],
+    ];
+
+    assert_eq!(Matrix::from(elements).elements, elements);
   }
 }
