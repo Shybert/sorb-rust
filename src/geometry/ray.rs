5b5342819709a8dcb9cf348582c388076d1ae863
Shybert <25749775+Shybert@users.noreply.github.com>
2020-07-29 08:35:44 +0200

    Ray: Renamed from() -> new()

--- src/geometry/ray.rs
+++ src/geometry/ray.rs
@@ -16,7 +16,7 @@ pub struct Ray {
   direction: Vector,
 }
 impl Ray {
-  pub fn from(origin: Point, direction: Vector) -> Self {
+  pub fn new(origin: Point, direction: Vector) -> Self {
     return Self { origin, direction };
   }
 
@@ -46,20 +46,20 @@ mod tests {
   fn get_origin() {
     let origin = Point::new(1., 2., 3.);
     let direction = Vector::new(4., 5., 6.);
-    let ray = Ray::from(origin, direction);
+    let ray = Ray::new(origin, direction);
     assert_eq!(ray.get_origin(), &origin);
   }
   #[test]
   fn get_direction() {
     let origin = Point::new(1., 2., 3.);
     let direction = Vector::new(4., 5., 6.);
-    let ray = Ray::from(origin, direction);
+    let ray = Ray::new(origin, direction);
     assert_eq!(ray.get_direction(), &direction);
   }
 
   #[test]
   fn position() {
-    let ray = Ray::from(Point::new(2., 3., 4.), Vector::new(1., 0., 0.));
+    let ray = Ray::new(Point::new(2., 3., 4.), Vector::new(1., 0., 0.));
     assert_eq!(ray.position(0.), Point::new(2., 3., 4.,));
     assert_eq!(ray.position(1.), Point::new(3., 3., 4.,));
     assert_eq!(ray.position(-1.), Point::new(1., 3., 4.,));
