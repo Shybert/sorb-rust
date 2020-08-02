f91687310964c6529ed03920e4e7d06f919cf6aa
Shybert <25749775+Shybert@users.noreply.github.com>
2020-08-02 10:27:21 +0200

    Added function for solving quadratic equations

--- src/utils.rs
+++ src/utils.rs
@@ -25,9 +25,26 @@ pub fn clamp_number(number: f64, min: f64, max: f64) -> f64 {
   };
 }
 
+pub fn quadratic(a: f64, b: f64, c: f64) -> Option<(f64, f64)> {
+  let discriminant = b.powi(2) - 4. * a * c;
+  if discriminant < 0. {
+    return None;
+  }
+
+  let (mut x1, mut x2) = (
+    (-b - discriminant.sqrt()) / (2. * a),
+    (-b + discriminant.sqrt()) / (2. * a),
+  );
+  if x1 > x2 {
+    std::mem::swap(&mut x1, &mut x2);
+  };
+  return Some((x1, x2));
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
+  use crate::assert_ae;
 
   #[test]
   fn clamp_number_above_max() {
@@ -52,5 +69,35 @@ mod tests {
   #[test]
   fn clamp_number_between() {
     assert_eq!(clamp_number(128., 0., 255.), 128.);
+  }
+
+  #[test]
+  fn quadratic_two_roots() {
+    let roots = quadratic(2., -5., 0.);
+    let (x1, x2) = roots.expect("Expected roots");
+    assert_ae!(x1, 0.);
+    assert_ae!(x2, 2.5);
+  }
+
+  #[test]
+  fn quadratic_single_root() {
+    let roots = quadratic(2., 4., 2.);
+    let (x1, x2) = roots.expect("Expected roots");
+    assert_ae!(x1, -1.);
+    assert_ae!(x2, -1.);
+  }
+
+  #[test]
+  fn quadratic_zero_roots() {
+    let roots = quadratic(2., 4., 9.);
+    assert!(roots.is_none());
+  }
+
+  #[test]
+  fn quadratic_large_floating_point_numbers() {
+    let roots = quadratic(-299.45, 392348.998, 889.000987);
+    let (x1, x2) = roots.expect("Expected roots");
+    assert_ae!(x1, -0.00227);
+    assert_ae!(x2, 1310.23435);
   }
 }
