c61defc2b7d1a8c572392cd42bb959cb7d996e09
Shybert <25749775+Shybert@users.noreply.github.com>
2020-08-04 09:38:04 +0200

    Added get_hit()

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,4 +1,5 @@
 use crate::geometry::Ray;
+use std::cmp::Ordering::Equal;
 
 mod sphere;
 pub use sphere::*;
@@ -16,6 +17,12 @@ impl Intersection {
     return Self { time };
   }
 }
+pub fn get_hit(intersections: &[Intersection]) -> Option<&Intersection> {
+  return intersections
+    .iter()
+    .filter(|intersection| intersection.time >= 0.)
+    .min_by(|x, y| x.time.partial_cmp(&y.time).unwrap_or(Equal));
+}
 
 #[cfg(test)]
 mod tests {
@@ -25,5 +32,38 @@ mod tests {
   fn intersection_init() {
     let intersection = Intersection::new(5.);
     assert_eq!(intersection.time, 5.)
+  }
+
+  #[test]
+  fn hit_when_all_positive() {
+    let intersections = vec![Intersection::new(1.), Intersection::new(2.)];
+    let hit = get_hit(&intersections).expect("Expected hit");
+    assert_eq!(hit.time, 1.);
+  }
+
+  #[test]
+  fn hit_when_some_negative() {
+    let intersections = vec![Intersection::new(-1.), Intersection::new(1.)];
+    let hit = get_hit(&intersections).expect("Expected hit");
+    assert_eq!(hit.time, 1.);
+  }
+
+  #[test]
+  fn hit_when_all_negative() {
+    let intersections = vec![Intersection::new(-2.), Intersection::new(-1.)];
+    let hit = get_hit(&intersections);
+    assert!(hit.is_none());
+  }
+
+  #[test]
+  fn hit_intersection_order_does_not_matter() {
+    let intersections = vec![
+      Intersection::new(5.),
+      Intersection::new(7.),
+      Intersection::new(-3.),
+      Intersection::new(2.),
+    ];
+    let hit = get_hit(&intersections).expect("Expected hit");
+    assert_eq!(hit.time, 2.);
   }
 }
