4a25afbb3edf7ae06e6af79d28b0fb7957151ab0
Shybert <25749775+Shybert@users.noreply.github.com>
2020-08-12 20:14:32 +0200

    Added Material struct

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,8 +1,49 @@
 use crate::geometry::{Interaction, Matrix, Point, Ray, Vector};
+use crate::Color;
 use std::cmp::Ordering::Equal;
 
 mod sphere;
 pub use sphere::*;
+
+pub struct Material {
+  color: Color,
+  ambience: f64,
+  diffuse: f64,
+  specular: f64,
+  shininess: f64,
+}
+impl Material {
+  fn new(color: Color, ambience: f64, diffuse: f64, specular: f64, shininess: f64) -> Self {
+    return Self {
+      color,
+      ambience,
+      diffuse,
+      specular,
+      shininess,
+    };
+  }
+
+  pub fn get_color(&self) -> &Color {
+    return &self.color;
+  }
+  pub fn get_ambience(&self) -> &f64 {
+    return &self.ambience;
+  }
+  pub fn get_diffuse(&self) -> &f64 {
+    return &self.diffuse;
+  }
+  pub fn get_specular(&self) -> &f64 {
+    return &self.specular;
+  }
+  pub fn get_shininess(&self) -> &f64 {
+    return &self.shininess;
+  }
+}
+impl Default for Material {
+  fn default() -> Self {
+    return Self::new(Color::white(), 0.1, 0.9, 0.9, 200.);
+  }
+}
 
 pub trait Shape {
   fn get_transformation(&self) -> &Matrix;
@@ -31,7 +72,32 @@ pub fn get_hit(intersections: &[Intersection]) -> Option<&Intersection> {
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::color::Color;
+
+  #[test]
+  fn material_init_new() {
+    let color = Color::red();
+    let ambience = 0.3;
+    let diffuse = 0.5;
+    let specular = 0.7;
+    let shininess = 40.;
+
+    let material = Material::new(color, ambience, diffuse, specular, shininess);
+    assert_eq!(material.get_color(), &color);
+    assert_eq!(material.get_ambience(), &ambience);
+    assert_eq!(material.get_diffuse(), &diffuse);
+    assert_eq!(material.get_specular(), &specular);
+    assert_eq!(material.get_shininess(), &shininess);
+  }
+
+  #[test]
+  fn material_init_default() {
+    let material = Material::default();
+    assert_eq!(material.get_color(), &Color::white());
+    assert_eq!(material.get_ambience(), &0.1);
+    assert_eq!(material.get_diffuse(), &0.9);
+    assert_eq!(material.get_specular(), &0.9);
+    assert_eq!(material.get_shininess(), &200.);
+  }
 
   #[test]
   fn intersection_init() {
