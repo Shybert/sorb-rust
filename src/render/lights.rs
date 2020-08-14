102a322998d2c08441798a7cba9d2f3a58b097b4
Shybert <25749775+Shybert@users.noreply.github.com>
2020-08-14 20:24:27 +0200

    Refactored Light struct
    * Renamed to PointLight
    * Derived basic traits
    * Implemented Default

--- src/render/lights.rs
+++ src/render/lights.rs
@@ -1,13 +1,14 @@
 use crate::geometry::Point;
 use crate::Color;
 
-pub struct Light {
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct PointLight {
   position: Point,
   color: Color,
 }
-impl Light {
+impl PointLight {
   pub fn new(position: Point, color: Color) -> Self {
-    return Self { position, color };
+    return PointLight { position, color };
   }
 
   pub fn position(&self) -> &Point {
@@ -17,17 +18,29 @@ impl Light {
     return &self.color;
   }
 }
+impl Default for PointLight {
+  fn default() -> Self {
+    return Self::new(Point::origin(), Color::white());
+  }
+}
 
 #[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
-  fn init() {
-    let position = Point::origin();
-    let color = Color::white();
-    let light = Light::new(position, color);
+  fn point_light_init_new() {
+    let position = Point::new(1., 2., 3.);
+    let color = Color::cyan();
+    let light = PointLight::new(position, color);
     assert_eq!(light.position(), &position);
     assert_eq!(light.color(), &color);
+  }
+
+  #[test]
+  fn point_light_init_default() {
+    let light = PointLight::default();
+    assert_eq!(light.position(), &Point::origin());
+    assert_eq!(light.color(), &Color::white());
   }
 }
