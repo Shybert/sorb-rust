35c717f6e76444111d2783fb761057d6313c42eb
Shybert <25749775+Shybert@users.noreply.github.com>
2020-08-16 07:50:46 +0200

    Scenes now have lights

--- src/render/scene.rs
+++ src/render/scene.rs
@@ -1,16 +1,22 @@
+use crate::render::PointLight;
 use crate::shapes::Shape;
 
 #[derive(Default)]
 pub struct Scene {
   objects: Vec<Box<dyn Shape>>,
+  lights: Vec<PointLight>,
 }
 impl Scene {
-  pub fn new(objects: Vec<Box<dyn Shape>>) -> Self {
-    return Self { objects };
+  pub fn new(objects: Vec<Box<dyn Shape>>, lights: Vec<PointLight>) -> Self {
+    return Self { objects, lights };
   }
 
   pub fn objects(&self) -> &[Box<dyn Shape>] {
     return &self.objects;
+  }
+
+  pub fn lights(&self) -> &[PointLight] {
+    return &self.lights;
   }
 }
 
@@ -21,16 +27,18 @@ mod tests {
 
   #[test]
   fn init_new() {
-    let scene = Scene::new(vec![
-      Box::new(Sphere::default()),
-      Box::new(Sphere::default()),
-    ]);
+    let scene = Scene::new(
+      vec![Box::new(Sphere::default()), Box::new(Sphere::default())],
+      vec![PointLight::default(); 3],
+    );
     assert_eq!(scene.objects().len(), 2);
+    assert_eq!(scene.lights().len(), 3);
   }
 
   #[test]
   fn init_default() {
     let scene = Scene::default();
     assert_eq!(scene.objects().len(), 0);
+    assert_eq!(scene.lights().len(), 0);
   }
 }
