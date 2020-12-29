c8a6bec1929350c3d7fb4bd668ea4b6e99009f14
Shybert <25749775+Shybert@users.noreply.github.com>
2020-12-29 10:47:20 +0100

    Expanded Camera class
    Canvas width and height is now provided on initialization. render() returns a new Canvas instead of rendering to a provided one.
    
    Camera-to-world transform now provided on initialization.

--- src/render/camera.rs
+++ src/render/camera.rs
@@ -3,21 +3,50 @@ use crate::render::{lighting, Canvas, World};
 use crate::shapes::find_hit;
 
 pub struct Camera {
-  pub fov: f64,
+  canvas_width: usize,
+  canvas_height: usize,
+  fov: f64,
+  camera_to_world: Matrix,
 }
 impl Camera {
-  pub fn render(&self, world: &World, canvas: &mut Canvas, transform: Matrix) {
+  pub fn new(canvas_width: usize, canvas_height: usize, fov: f64, camera_to_world: Matrix) -> Self {
+    return Self {
+      canvas_width,
+      canvas_height,
+      fov,
+      camera_to_world,
+    };
+  }
+
+  pub fn canvas_width(&self) -> usize {
+    return self.canvas_width;
+  }
+  pub fn canvas_height(&self) -> usize {
+    return self.canvas_height;
+  }
+
+  pub fn fov(&self) -> f64 {
+    return self.fov;
+  }
+
+  pub fn camera_to_world(&self) -> &Matrix {
+    return &self.camera_to_world;
+  }
+
+  pub fn render(&self, world: &World) -> Canvas {
+    let mut canvas = Canvas::new(self.canvas_width(), self.canvas_height());
+
     let width = canvas.width() as f64;
     let height = canvas.height() as f64;
-
-    let aspect_ratio = width / height;
-    let computed_fov = (self.fov.to_radians() / 2.).tan();
+    let aspect_ratio = canvas.aspect_ratio();
+    let computed_fov = (self.fov().to_radians() / 2.).tan();
 
     for i in 0..canvas.width() {
       for j in 0..canvas.height() {
         let x = (-1. + 2. * ((i as f64 + 0.5) / width)) * aspect_ratio * computed_fov;
         let y = (1. - 2. * ((j as f64 + 0.5) / height)) * computed_fov;
-        let ray = transform * Ray::new(Point::new(0., 0., 0.), Vector::new(x, y, -1.));
+        let ray =
+          *self.camera_to_world() * Ray::new(Point::new(0., 0., 0.), Vector::new(x, y, -1.));
 
         let intersections = world.intersect(&ray);
         let hit = find_hit(&intersections);
@@ -35,5 +64,38 @@ impl Camera {
         }
       }
     }
+
+    return canvas;
+  }
+}
+impl Default for Camera {
+  fn default() -> Self {
+    return Camera::new(640, 640, 90., Matrix::identity());
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn init_new() {
+    let camera = Camera::new(400, 320, 90., Matrix::identity().scale(-1., 1., -1.));
+    assert_eq!(camera.canvas_width(), 400);
+    assert_eq!(camera.canvas_height(), 320);
+    assert_eq!(camera.fov(), 90.);
+    assert_eq!(
+      camera.camera_to_world(),
+      &Matrix::identity().scale(-1., 1., -1.)
+    );
+  }
+
+  #[test]
+  fn init_default() {
+    let camera = Camera::default();
+    assert_eq!(camera.canvas_width(), 640);
+    assert_eq!(camera.canvas_height(), 640);
+    assert_eq!(camera.fov(), 90.);
+    assert_eq!(camera.camera_to_world(), &Matrix::identity());
   }
 }
