7b55850cd261497a08c3547ab8f06354774ac357
Shybert <25749775+Shybert@users.noreply.github.com>
2020-12-30 10:18:06 +0100

    Derived PartialEq for Ray

--- src/geometry/ray.rs
+++ src/geometry/ray.rs
@@ -1,6 +1,6 @@
 use crate::geometry::{Point, Vector};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Ray {
   pub origin: Point,
   pub direction: Vector,
@@ -35,5 +35,23 @@ mod tests {
     assert_eq!(ray.position(1.), Point::new(3., 3., 4.,));
     assert_eq!(ray.position(-1.), Point::new(1., 3., 4.,));
     assert_eq!(ray.position(2.5), Point::new(4.5, 3., 4.,));
+  }
+
+  #[test]
+  fn equality_identical() {
+    let origin = Point::new(1., 0., 1.);
+    let direction = Vector::new(1., 1., 1.);
+    let a = Ray::new(origin, direction);
+    let mut b = Ray::new(origin, direction);
+
+    assert_eq!(a, b);
+    b.origin.x = 1.0000001;
+    assert_eq!(a, b);
+  }
+  #[test]
+  fn equality_different() {
+    let a = Ray::new(Point::new(-1., -2., -3.), Vector::new(1., -1., 1.));
+    let b = Ray::new(Point::new(5., 8., -3.), Vector::new(3., 2., 1.));
+    assert_ne!(a, b);
   }
 }
