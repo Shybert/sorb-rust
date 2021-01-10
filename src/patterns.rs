e4f3fa18e04ee183ffe853815f9e7c137b0454be
Shybert <25749775+Shybert@users.noreply.github.com>
2021-01-10 14:52:48 +0100

    Implemented Pattern for Color
    Color can now be used as a pattern that is only ever a single color.

--- src/patterns.rs
+++ src/patterns.rs
@@ -10,3 +10,33 @@ pub trait Pattern: Debug {
 
   fn color_at(&self, point: &Point) -> Color;
 }
+
+impl Pattern for Color {
+  fn get_colors(&self) -> Vec<&Color> {
+    return vec![self];
+  }
+
+  fn color_at(&self, _point: &Point) -> Color {
+    return *self;
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn color_get_colors() {
+    let color = Color::cyan();
+    assert_eq!(color.get_colors()[0], &Color::cyan());
+  }
+
+  #[test]
+  fn color_color_at_is_constant() {
+    let color = Color::default();
+    assert_eq!(color.color_at(&Point::origin()), Color::black());
+    assert_eq!(color.color_at(&Point::new(-15., 30., 8.)), Color::black());
+    assert_eq!(color.color_at(&Point::new(1.36, -32., 12.)), Color::black());
+    assert_eq!(color.color_at(&Point::new(0., 33.33, -5.7)), Color::black());
+  }
+}
